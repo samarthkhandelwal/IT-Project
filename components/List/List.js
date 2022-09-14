93d5b5a955d56096821d6d205c32d1f7e55c8282
hayzach <90233125+hayzach@users.noreply.github.com>
2022-09-14 21:29:53 +1000

    Changed the filter muscles text input to a dropdown button

--- components/List/List.js
+++ components/List/List.js
@@ -3,9 +3,11 @@ import styles from "../../styles/List.module.css";
 // React Bootstrap Components
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 
 export default function List({ list, selected, setSelected }) {
   return (
@@ -13,10 +15,21 @@ export default function List({ list, selected, setSelected }) {
       {/* The Search and Filter Form */}
       <InputGroup className="mb-3">
         <Form.Control aria-label="First name" placeholder="Search" />
-        <Form.Control aria-label="Last name" placeholder="Filter" />
+
+        <Dropdown as={ButtonGroup}>
+          <Button variant="primary">Filter</Button>
+
+          <Dropdown.Toggle split variant="primary" id="dropdown-split-basic" />
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Chest</Dropdown.Item>
+            <Dropdown.Item>Back</Dropdown.Item>
+            <Dropdown.Item>Hamstrings</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </InputGroup>
 
-      {/* The list. A group of toggle buttons, so that */}
+      {/* The list. A group of toggle buttons, so that the active one can be kept track of*/}
       <div className={styles.container}>
         <ToggleButtonGroup vertical name="bob">
           {list.map((element) => (
