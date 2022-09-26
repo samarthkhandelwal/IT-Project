22916baaf30f35c0032b188bb5fab60cfed070f7
Duvindra Dissanayake <d.dissanayake@student.unimelb.edu.au>
2022-09-26 16:56:32 +1000

    link verification implemented.

--- components/YouTube.js
+++ components/YouTube.js
@@ -1,10 +1,18 @@
-function YouTube() {
+function verifyURL(link) {
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = link.match(regExp);
+  return (match&&match[7].length==11)? match[7] : false;
+}
+
+function YouTube({link}) {
+  var id = verifyURL(link)
+
   return (
     <>
-      <iframe
-        width="100%"
-        height="60%"
-        src="https://www.youtube.com/embed/TwD-YGVP4Bk"
+      <iframe 
+      width="100%"
+      height="60%"
+      src={"https://www.youtube.com/embed/" + id} 
       ></iframe>
     </>
   );
