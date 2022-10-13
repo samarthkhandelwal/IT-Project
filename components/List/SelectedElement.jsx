457b427bea287b43c6b9d6a321224d4456f916b9
Duvindra Dissanayake <d.dissanayake@student.unimelb.edu.au>
2022-10-14 08:39:12 +1100

    favorite icon added to selected exercise.
    slight img bug to be fixed.

--- components/List/SelectedElement.jsx
+++ components/List/SelectedElement.jsx
@@ -1,16 +1,39 @@
 // Import React
-import React from 'react';
+import React, { useState } from 'react';
 
 // Styles
 import styles from '../../styles/List.module.css';
 
+const star = '/images/star.png';
+const starFilled = '/images/starFilled.png';
+
 // Element returns what should be displayed for each element of the list
 export default function Element({ element }) {
+  const [imgPath, setImgPath] = useState(star);
+  
+  const toggleStar = (e) => {
+    e.preventDefault();
+    imgPath == star ? setImgPath(starFilled) : setImgPath(star);
+  };
+
   return (
     <div className={styles.selement}>
       <div className={styles.stxt}>
         <h1>{element.name}</h1>
         <p>{element.muscleGroups.join(', ')}</p>
+      </div>
+
+      <div className={styles.star}>
+        <form>
+          <input
+            type="image"
+            src={imgPath}
+            height={38}
+            width={38}
+            alt="star"
+            onClick={toggleStar}
+          ></input>
+        </form>
       </div>
     </div>
   );
