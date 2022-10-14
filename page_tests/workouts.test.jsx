7881a16a35c136e00d9a793cce231c997407d65e
Hu90mt990 <hmonacotempl@student.unimelb.edu.au>
2022-10-15 01:35:13 +1100

    Implemented favourites
    Favourites for exercises and workouts are now stored in the user doc and sorted to the top of the workout and exercise pages

--- page_tests/workouts.test.jsx
+++ page_tests/workouts.test.jsx
@@ -1,44 +1,9 @@
 // React
-import React, { useState } from 'react';
+import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 
 // Custom Components
-//import WorkoutsPage from '../pages/workouts/index';
-
-import Workout from '../public/classes/Workout';
-
-import List from '../components/List/List';
-
-import styles from '../styles/Workouts.module.css';
-
-function WorkoutsTest() {
-  // A dummy workout list so that we have data to render.
-  // Once the database is implemented this will not be necessary
-  const workoutList = [];
-  workoutList.push(
-    new Workout('Push Workout', ['Chest', 'Shoulder', 'Triceps']),
-    new Workout('Pull Workout', ['Back', 'Biceps', 'Abs']),
-    new Workout('Legs Workout', ['Quadriceps', 'Hamstrings', 'Calves']),
-    new Workout('Upper Workout', ['Chest', 'Back', 'Shoulder', 'Triceps']),
-    new Workout('Workout 1', ['Chest', 'Shoulder', 'Triceps']),
-    new Workout('Workout 2', ['Back', 'Biceps', 'Abs']),
-    new Workout('Workout 3', ['Quadriceps', 'Hamstrings', 'Calves']),
-    new Workout('Workout 4', ['Chest', 'Back', 'Shoulder', 'Triceps'])
-  );
-
-  const selectState = {};
-  [selectState.selected, selectState.setSelected] = useState('');
-
-  return (
-    <>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <List list={workoutList} listType="radio" {...selectState} />
-        </main>
-      </div>
-    </>
-  );
-}
+import WorkoutsTest from './WorkoutsTest';
 
 describe('The List of buttons displaying Workouts or Exercises', () => {
   it('Updates the selected button when another is clicked', () => {
--- pages/workouts/index.jsx
+++ pages/workouts/index.jsx
@@ -12,21 +12,34 @@ import List from '../../components/List/List';
 // Styles
 import styles from '../../styles/Workouts.module.css';
 
-// Import the database reference and functions for reading from firestore
+// Authentication
+import { useAuth } from '../../context/authUserContext';
 
 // Get reference to workouts collection
 const workoutsCollectionRef = collection(db, 'workouts');
 
 export default function WorkoutsPage() {
   const [workoutList, setWorkoutList] = useState([]);
+  const { authUser } = useAuth();
   useEffect(() => {
     const getWorkouts = async () => {
       const q = query(workoutsCollectionRef, orderBy('name'), limit(10));
       const data = await getDocs(q);
-      setWorkoutList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      const workouts = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      if (authUser) {
+        const favs = workouts.filter((doc) =>
+          authUser.favouriteWorkouts.includes(doc.id)
+        );
+        const unfavs = workouts.filter(
+          (doc) => !authUser.favouriteWorkouts.includes(doc.id)
+        );
+        setWorkoutList(favs.concat(unfavs));
+      } else {
+        setWorkoutList(workouts);
+      }
     };
     getWorkouts();
-  }, []);
+  }, [authUser]);
 
   const [selected, setSelected] = useState('');
 
@@ -40,6 +53,7 @@ export default function WorkoutsPage() {
             listType="radio"
             selected={selected}
             setSelected={setSelected}
+            type="workouts"
           />
         </main>
       </div>
