180137668ff5ae864f916711c74f1577163c6d55
Hu90mt990 <hmonacotempl@student.unimelb.edu.au>
2022-10-16 16:57:27 +1100

    Implement database data population

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -4,24 +4,7 @@ import React from 'react';
 // Styles
 import styles from '../styles/Instructions.module.css';
 
-export default function Instructions() {
-  const text = `
-    Step 1: Stand up straight with your torso upright. Hold a dumbbell in
-    each hand at arms-length. Your elbows should be close to your torso.
-  
-    Step 2: The palms of your hands should be facing your torso. This is
-    the starting position for the exercise.
-  
-    Step 3: Curl the weight forward while contracting your biceps. Your
-    upper arm should remain stationary. Continue to lift the weight until
-    your biceps are fully contracted and the dumbbell is at shoulder
-    level. Hold the contraction for a moment as you squeeze your biceps.
-  
-    Step 4: Inhale and slowly start to bring the dumbbells back to the
-    starting position.
-
-    Step 5: Repeat for the desired number of reps.`;
-
+export default function Instructions({ text }) {
   return (
     <div className={styles.instructions}>
       <span className={styles.line}>{text}</span>
--- pages/exercises/index.jsx
+++ pages/exercises/index.jsx
@@ -5,6 +5,10 @@ import React, { useState, useEffect } from 'react';
 import { getDocs, collection, query, orderBy, limit } from 'firebase/firestore';
 import { db } from '../../firebase-config';
 
+import Instructions from '../../components/Instructions';
+import YouTube from '../../components/YouTube';
+import { Container, Row, Col } from 'react-bootstrap';
+
 // Custom components
 import List from '../../components/List/List';
 import TopNavbar from '../../components/Navbar/Navbar';
@@ -20,6 +24,7 @@ const exercisesCollectionRef = collection(db, 'exercises');
 
 export default function ExercisesPage() {
   const [exerciseList, setExerciseList] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState();
   const { authUser } = useAuth();
   useEffect(() => {
     const getExercises = async () => {
@@ -33,28 +38,64 @@ export default function ExercisesPage() {
         const unfavs = exercises.filter(
           (doc) => !authUser.favouriteExercises.includes(doc.id)
         );
-        setExerciseList(favs.concat(unfavs));
+        const finalList = favs.concat(unfavs);
+        setSelectedExercise(finalList[0]);
+        setExerciseList(finalList);
+        
       } else {
+        setSelectedExercise(exercises[0]);
         setExerciseList(exercises);
+        
       }
     };
     getExercises();
   }, [authUser]);
   const [selected, setSelected] = useState('');
+  
+
+  useEffect(() => {
+    const getSelected = () => {
+    if (selected){
+            exerciseList.forEach((doc) => {
+                if (doc.id === selected){
+                    setSelectedExercise(doc);
+                    return;
+                }
+            })
+        }
+    }
+    getSelected();
+  }, [selected,exerciseList])
+
   return (
     <>
       <TopNavbar />
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <List
-            list={exerciseList}
-            listType="radio"
-            selected={selected}
-            setSelected={setSelected}
-            type="exercises"
-          />
-        </main>
-      </div>
+      <Container className={styles.container}>
+        <Row>
+          <Col>
+            {selectedExercise != null && 
+                <YouTube link={selectedExercise.videoURL} />
+            }
+            {selectedExercise != null && 
+                <Instructions text={selectedExercise.instructions}/>
+            }
+          </Col>
+
+          <Col>
+            <div>
+              <main className={styles.main}>
+                <List
+                  list={exerciseList}
+                  listType="radio"
+                  selected={selected}
+                  setSelected={setSelected}
+                  type="exercises"
+                />
+              </main>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
