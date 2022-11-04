6f01ce9bd25253ef67e889ac51286c3340cda3ef
Rory Healy <roryihealy@outlook.com>
2022-11-04 17:34:12 +1100

    basic support for creating user workouts

---
+++ pages/userworkouts/create/index.jsx
@@ -0,0 +1,344 @@
+// React
+import React, { useEffect, useRef, useState } from 'react';
+
+// Next components
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+// Bootstrap components
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+// Firebase
+import {
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  getDocs,
+  updateDoc,
+  doc,
+} from 'firebase/firestore';
+import { db } from '../../../firebase-config';
+
+// Custom components
+import AddExerciseModal from '../../../components/WorkoutForms/AddExerciseModal';
+import CustomAlert from '../../../components/EditButton/CustomAlert';
+import EditExerciseModal from '../../../components/WorkoutForms/EditExerciseModal';
+import ExerciseElement from '../../../components/WorkoutForms/ExerciseElement';
+import TopNavbar from '../../../components/Navbar/Navbar';
+
+// Styles
+import styles from '../../../styles/WorkoutForms/WorkoutForm.module.css';
+
+// Authentication
+import { useAuth } from '../../../context/authUserContext';
+
+// Get reference to exercises and workouts collections
+const storedWorkoutsCollectionRef = collection(db, 'storedWorkouts');
+const exercisesCollectionRef = collection(db, 'exercises');
+const usersCollectionRef = collection(db, 'users');
+
+function WorkoutForm() {
+  const router = useRouter();
+  const { authUser } = useAuth();
+
+  /* Ensures that the database is only queried once for data */
+  const isFirstLoad = useRef(false);
+
+  /* Handles state for validation of form */
+  // TODO: Form validation
+  // const [validated, setValidated] = useState(false);
+
+  /* Handles state for the alert */
+  const [isAlertActive, setAlertActive] = useState({});
+  const handleAlertOpen = ({ heading, body, variant }) => {
+    setAlertActive({ heading, body, variant });
+  };
+  const handleAlertClose = () => {
+    setAlertActive({});
+  };
+
+  /* Used to store the ids of exercises that have been added to the workout. */
+  const [exerciseGroups, setExerciseGroups] = useState([]);
+
+  /* Used to store a list of the exercises that can be included in the workout */
+  const [exercises, setExercises] = useState(undefined);
+
+  /* Used to store the selected exercise in the 'Add new exercise' modal */
+  const [selectedExercise, setSelectedExercise] = useState({});
+
+  useEffect(() => {
+    const getExercises = async () => {
+      const q = query(exercisesCollectionRef, orderBy('name'));
+      const data = await getDocs(q);
+      setExercises(data.docs.map((d) => ({ ...d.data(), id: d.id })));
+    };
+
+    if (!isFirstLoad.current) {
+      getExercises();
+      isFirstLoad.current = true;
+    }
+  }, []);
+
+  /* Keeps track of which index the exercise is in the workout */
+  const index = useRef(0);
+
+  const updateExercises = (ex) => {
+    setExerciseGroups(exerciseGroups.concat(ex));
+    setSelectedExercise({});
+  };
+
+  const deleteExercise = (toDelete) => {
+    const newGroups = exerciseGroups.filter((ex) => ex.index !== toDelete);
+    for (let i = 0; i < exerciseGroups.length - 1; i += 1) {
+      newGroups[i].index = i;
+    }
+    setExerciseGroups(newGroups);
+    index.current -= 1;
+  };
+
+  /* Handles state for the add exercise modal */
+  const [isAddExerciseModalOpen, setAddExerciseModalOpen] = useState(false);
+  const handleAddExerciseModalOpen = () => setAddExerciseModalOpen(true);
+  const handleAddExerciseModalClose = () => {
+    /* This check is to differentiate between cancelling or actually adding an exercise */
+    if (selectedExercise.id) {
+      updateExercises({
+        ...selectedExercise,
+        index: index.current,
+        sets: 0,
+        reps: 0,
+      });
+      index.current += 1;
+    }
+    setAddExerciseModalOpen(false);
+  };
+
+  /* Handles state for the editing sets/reps for an exercise modal */
+  const [isEditExerciseModalOpen, setEditExerciseModalOpen] = useState(false);
+  const handleEditExerciseModalOpen = (ex) => {
+    setSelectedExercise(ex);
+    setEditExerciseModalOpen(true);
+  };
+  const handleEditExerciseModalClose = () => {
+    for (let i = 0; i < exerciseGroups.length; i += 1) {
+      if (exerciseGroups[i].id === selectedExercise.id) {
+        exerciseGroups[i].sets = selectedExercise.sets;
+        exerciseGroups[i].reps = selectedExercise.reps;
+        break;
+      }
+    }
+    setExerciseGroups(exerciseGroups);
+    setEditExerciseModalOpen(false);
+  };
+
+  /* Transforms an exercise into the correct data structure for the form submission */
+  const getRepsSetsMuscles = () => {
+    const exercisesList = [];
+    const muscleGroupsList = [];
+
+    for (let i = 0; i < exerciseGroups.length; i += 1) {
+      /* Not sure why, but I can't seem to access the data directly here.
+       * So this is a workaround.
+       */
+      const obj = { ...exerciseGroups[i] };
+      delete obj.instructions;
+      delete obj.equipment;
+      delete obj.videoURL;
+      delete obj.index;
+      delete obj.muscleGroups;
+
+      /* Get the muscle groups from the exercises */
+      if (exercises) {
+        for (let j = 0; j < exercises.length; j += 1) {
+          if (exercises[j].id === obj.id) {
+            for (let k = 0; k < exercises[j].muscleGroups.length; k += 1) {
+              if (!muscleGroupsList.includes(exercises[j].muscleGroups[k])) {
+                muscleGroupsList.push(exercises[j].muscleGroups[k]);
+              }
+            }
+            break;
+          }
+        }
+      }
+
+      exercisesList.push(obj);
+    }
+
+    return [exercisesList, muscleGroupsList.sort()];
+  };
+
+  const updateUserDoc = async (docRef) => {
+    authUser.createdWorkouts.push(docRef);
+    await updateDoc(doc(usersCollectionRef, authUser.uid), {
+      createdWorkouts: authUser.createdWorkouts,
+    });
+  };
+
+  /* Handles the submission of forms. */
+  const handleSubmit = async (event) => {
+    /* Prevent automatic submission and refreshing of the page. */
+    event.preventDefault();
+
+    const [exercisesList, muscleGroups] = getRepsSetsMuscles(event.target);
+
+    const data = {
+      name: event.target.workoutName.value,
+      muscleGroups,
+      exercises: exercisesList,
+    };
+
+    /* Send the form data to the API and get a response */
+    const response = await fetch('/api/userworkouts', {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+    /* Get the response, add the document, create an alert, then redirect. */
+    const result = await response.json();
+    if (result.error) {
+      handleAlertOpen({
+        heading: 'Error',
+        body: result.error,
+        variant: 'danger',
+      });
+      return;
+    }
+
+    const docRef = await addDoc(storedWorkoutsCollectionRef, result.data)
+      .then(() => {
+        handleAlertOpen({
+          heading: 'Success!',
+          body: `${result.data.name} was added to your workout list. Redirecting...`,
+          variant: 'success',
+        });
+        setTimeout(() => {
+          updateUserDoc(docRef);
+          router.push('/userworkouts');
+        }, 3000);
+      })
+      .catch((error) => {
+        handleAlertOpen({
+          heading: 'Error',
+          body: `${error.name}: ${error.code}`,
+          variant: 'danger',
+        });
+      });
+  };
+
+  const displayAlert = ({ heading, body, variant }) => {
+    // TODO: Check if dismissible on error
+    if (heading && body && variant) {
+      return (
+        <CustomAlert
+          heading={heading}
+          body={body}
+          variant={variant}
+          onClose={handleAlertClose}
+        />
+      );
+    }
+    return null;
+  };
+
+  /* Handles the submission of forms to edit sets/reps of an exercise */
+  const handleEdit = (event) => {
+    event.preventDefault();
+    selectedExercise.sets = event.target.newsets.value;
+    selectedExercise.reps = event.target.newreps.value;
+    handleEditExerciseModalClose();
+  };
+
+  return (
+    <div className={styles.form}>
+      <div>
+        <h2>Creating new user workout</h2>
+      </div>
+
+      <Form onSubmit={handleSubmit} action="/api/workout" method="post">
+        <div className="mt-3 mb-3">
+          <Form.Label>Enter workout name:</Form.Label>
+          <Form.Control
+            id="workoutName"
+            type="text"
+            placeholder="Enter workout name"
+          />
+        </div>
+
+        <Form.Group>
+          <Form.Label>Exercises in this workout:</Form.Label>
+          <div className={styles.formexercises}>
+            {exerciseGroups.length === 0 ? (
+              <p>None</p>
+            ) : (
+              exerciseGroups.map((ex) => (
+                <ExerciseElement
+                  exercise={ex}
+                  onClick={() => {
+                    handleEditExerciseModalOpen(ex);
+                  }}
+                />
+              ))
+            )}
+          </div>
+        </Form.Group>
+
+        <div className={styles.addexercise}>
+          <Button
+            variant="outline-primary"
+            onClick={handleAddExerciseModalOpen}
+            className="mt-3 mb-3"
+          >
+            + Add an exercise
+          </Button>
+        </div>
+
+        {displayAlert(isAlertActive)}
+
+        <div className={`mt-3 ${styles.buttongroup}`}>
+          <Link href="/workouts" passHref>
+            <Button variant="secondary" size="lg">
+              Cancel
+            </Button>
+          </Link>
+          <Button variant="primary" type="submit" size="lg">
+            Submit
+          </Button>
+        </div>
+      </Form>
+
+      <AddExerciseModal
+        show={isAddExerciseModalOpen}
+        onClose={handleAddExerciseModalClose}
+        list={exercises}
+        setSelectedExercise={setSelectedExercise}
+      />
+
+      <EditExerciseModal
+        show={isEditExerciseModalOpen}
+        onClose={handleEditExerciseModalClose}
+        exercise={selectedExercise}
+        onSubmit={handleEdit}
+        onDelete={() => {
+          deleteExercise(selectedExercise.index);
+          setEditExerciseModalOpen(false);
+        }}
+      />
+    </div>
+  );
+}
+
+export default function CreateWorkout() {
+  return (
+    <>
+      <TopNavbar />
+      <div className={styles.main}>
+        <WorkoutForm />
+      </div>
+    </>
+  );
+}
